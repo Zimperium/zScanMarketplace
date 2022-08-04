9b100dc49ecf4f9c7a1f4996d79d9abb763ace7b
mthibeau73 <mthibeau@gmail.com>
2022-08-04 14:47:12 -0500

    Updated example sarif to include versionControlProvenance including the branch name.

--- src/action.js
+++ src/action.js
@@ -13,13 +13,16 @@ core.debug(`app: ${appFile}`);
 
 const sampleJson =
     '{"$schema":"https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",' +
-    '"version":"2.1.0","runs":[{"tool":{"driver":{"name":"Zimperium zScan Analysis","semanticVersion":"0.0",' +
+    '"version":"2.1.0",' +
+    '"runs":[{"versionControlProvenance":[{"repositoryUri":"https://github.com/Zimperium/zScanExampleApp",' +
+    '"branch":"Testing123"}],"tool":{"driver":{"name":"Zimperium zScan Analysis","semanticVersion":"0.0",' +
     '"informationUri":"https://ziap.zimperium.com","rules":[{"id":"5d8dde2c-d68f-b895-195f-561200000000",' +
     '"name":"Sample Rule 1","shortDescription":{"text":"Sample Short Description"},' +
-    '"fullDescription":{"text":"Sample Full Description"},"properties":{"tags":["Sample Tag 1"],"severity":"Low",' +
-    '"type":"privacy","category":"Personal Data","subcategory":"Logging"}}],"properties":{"appVersion":"2.0"}}},' +
-    '"results":[{"ruleId":"R1","ruleIndex":0,"message":{"text":"Sample Result Text"},' +
-    '"locations":[{"physicalLocation":{"artifactLocation":{"uri":"Location"},"region":{"startLine":133,' +
+    '"fullDescription":{"text":"Sample Full Description"},"properties":{"tags":["Sample Tag 1"],' +
+    '"severity":"Low","type":"privacy","category":"Personal Data","subcategory":"Logging"}}],' +
+    '"properties":{"appVersion":"2.0"}}},"results":[{"ruleId":"R1","ruleIndex":0,' +
+    '"message":{"text":"Sample Result Text"},' +
+    '"locations":[{"physicalLocation":{"artifactLocation":{"uri":"Location2"},"region":{"startLine":163,' +
     '"snippet":{"text":"Text"}}}}]}]}]}'
 
 core.setOutput( "sarifJson", sampleJson);
